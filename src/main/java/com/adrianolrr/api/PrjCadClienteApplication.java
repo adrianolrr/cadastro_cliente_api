60c68589881f5100dc15ef116e57a5bab5b32ee4
adrianolrr <adriano.lrr@gmail.com>
2019-03-18 12:31:56 -0300

    V1

---
+++ src/main/java/com/adrianolrr/api/PrjCadClienteApplication.java
@@ -0,0 +1,13 @@
+package com.adrianolrr.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjCadClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjCadClienteApplication.class, args);
+	}
+
+}
